6675c8aeb5dee4415818140af7b243b25e49e4fe
Fábio Berbert de Paula <fberbert@gmail.com>
2024-04-19 19:09:39 -0300

    layout improvments

--- components/about.tsx
+++ components/about.tsx
@@ -27,48 +27,48 @@ export default function About() {
 
       <p className="mb-1">Key Creations and Contributions:</p>
 
-      <ul className="sm:mb-3 sm:ml-9 list-none border-4 border-gray-400 dark:border-gray-900 rounded-md">
-        <li className="bg-gray-400 dark:bg-gray-900 p-4">
-          <FaRegCheckSquare className="inline mr-2" />
+      <ul className="sm:mb-3 sm:ml-9 list-none border-4 border-white border-opacity-60 dark:border-gray-900 rounded-md">
+        <li className="bg-white bg-opacity-60 dark:bg-gray-900 p-4">
+          <FaRegCheckSquare className="inline mr-2 text-blue-700 dark:text-blue-900" />
           {" "}
           Founder of <a target="_blank" href="https://vivaolinux.com.br" className="underline">Viva o Linux</a> - A renowned website in the Linux community.
         </li>
 
-        <li className="p-4">
-          <FaRegCheckSquare className="inline mr-2" />
+        <li className="p-4 bg-white bg-opacity-30 dark:bg-gray-900 dark:bg-opacity-30">
+          <FaRegCheckSquare className="inline mr-2 text-blue-700 dark:text-blue-900" />
           {" "}
           <a target="_blank" href="https://automatizando.dev" className="underline">Automatizando.dev</a> - A hub for my innovations in home automation and AI.
         </li>
-        <li className="bg-gray-400 dark:bg-gray-900 p-4">
-          <FaRegCheckSquare className="inline mr-2" />
+        <li className="bg-white bg-opacity-60 dark:bg-gray-900 p-4">
+          <FaRegCheckSquare className="inline mr-2 text-blue-700 dark:text-blue-900" />
           {" "}
           Inventor of <a target="_blank" href="https://automatizando.dev/brinquedo" className="underline">Talking Toy</a> - An interactive toy integrating Alexa skills and AI.
         </li>
-        <li className="p-4">
-          <FaRegCheckSquare className="inline mr-2" />
+        <li className="p-4 bg-white bg-opacity-30 dark:bg-gray-900 dark:bg-opacity-30">
+          <FaRegCheckSquare className="inline mr-2 text-blue-700 dark:text-blue-900" />
           {" "}
           Developer of <a target="_blank" href="https://automatizando.dev/garagem" className="underline">Abrir Garagem</a> - A smart solution for garage door automation.
         </li>
-        <li className="bg-gray-400 dark:bg-gray-900 p-4">
-          <FaRegCheckSquare className="inline mr-2" />
+        <li className="bg-white bg-opacity-60 dark:bg-gray-900 p-4">
+          <FaRegCheckSquare className="inline mr-2 text-blue-700 dark:text-blue-900" />
           {" "}
           Instagram Content Creator: <a target="_blank" href="https://instagram.com/alexabolada" className="underline">@alexabolada</a> - Sharing insights and developments in AI and home automation.
         </li>
-        <li className="p-4">
-          <FaRegCheckSquare className="inline mr-2" />
+        <li className="p-4 bg-white bg-opacity-30 dark:bg-gray-900 dark:bg-opacity-30">
+          <FaRegCheckSquare className="inline mr-2 text-blue-700 dark:text-blue-900" />
           {" "}
           Expert in Cryptocurrency: Active in the field since 2016, with a focus
           on emerging technologies.
         </li>
-        <li className="bg-gray-400 dark:bg-gray-900 p-4">
-          <FaRegCheckSquare className="inline mr-2" />
+        <li className="bg-white bg-opacity-60 dark:bg-gray-900 p-4">
+          <FaRegCheckSquare className="inline mr-2 text-blue-700 dark:text-blue-900" />
           {" "}
           Free Software Community Contributor: Since 2002, contributing to
           various open-source projects.
         </li>
       </ul>
 
-      <p className="mt-6 mb-3 text-base text-center bg-gray-300 dark:bg-gray-800 p-3 rounded-full">
+      <p className="mt-6 mb-3 text-base text-center bg-blue-900 text-white dark:bg-gray-800 p-3 rounded-md">
         I am currently open for a{" "}
         <span className="font-medium">full-time position</span> as a software
         developer / devops engineer.
--- components/project.tsx
+++ components/project.tsx
@@ -31,10 +31,10 @@ export default function Project({
       }}
       className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+      <section className="bg-white max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative hover:bg-gray-100 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
-          <p className="mt-2 mb-4 leading-relaxed text-gray-900 dark:text-white/70">
+          <p className="mt-2 mb-4 leading-relaxed text-black dark:text-white/70">
             {description}
           </p>
           <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto justify-center sm:justify-start">
@@ -50,7 +50,7 @@ export default function Project({
           <div className="text-center p-4 mt-4 mb-1">
             <a
               href={url}
-              className="text-sm font-semibold hover:no-underline bg-blue-700 dark:bg-blue-900 px-4 py-2 rounded-full text-white dark:text-blue-100 hover:bg-blue-100 dark:hover:bg-blue-800 transition"
+              className="text-sm font-semibold hover:no-underline bg-blue-900 dark:bg-blue-900 px-4 py-2 rounded-full text-white dark:text-blue-100 hover:bg-blue-100 dark:hover:bg-blue-800 transition"
             >
               View project
             </a>
