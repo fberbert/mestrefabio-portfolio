cefd301caf88c391a31143d9ba63bded34fc18e7
Fábio Berbert de Paula <fberbert@gmail.com>
2024-04-26 05:00:48 -0300

    rounded border ao music-video

--- components/music-video.tsx
+++ components/music-video.tsx
@@ -8,7 +8,7 @@ export default function MusicVideo() {
         <link rel="preload" href="/video/fabio-music-redux.mp4" as="video" type="video/mp4" />
       </Head>
       <div className="relative w-full h-[56.25vw] padding-top-[56.25%] sm:padding-top-0 sm:h-[403px] my-3">
-        <video className="absolute top-0 left-0 w-full h-full sm:h-[auto]
+        <video className="absolute top-0 left-0 w-full h-full sm:h-[auto] rounded-3xl
           object-contain object-center" controls>
           <source src="/video/fabio-music-redux.mp4" type="video/mp4" />
           Your browser does not support the video tag.
